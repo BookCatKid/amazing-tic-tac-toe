d0620bc514a9e979ad98eafb0b3fd9492bc74857
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2026-01-09 18:57:25 -0800

    test fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '@/pages/Home';
 import { GamePage } from '@/pages/GamePage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game/:mode" element={<GamePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
